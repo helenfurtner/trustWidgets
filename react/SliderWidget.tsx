2c962d7fa1b41042a98d48257799237e748bfc48
Tina <suporte@geradordeideias.com>
2022-06-01 14:16:38 -0300

    Retirada de código

--- react/SliderWidget.tsx
+++ react/SliderWidget.tsx
@@ -2,40 +2,6 @@ import * as React from "react";
 
 const SliderWidget = () => 
 
-<div id="_trustvox_colt">
-    <div className="ts-store-opinions-widget">
-        <div className="ts-slider">
-            <div className="ts-frame">
-                <ul className="ts-slides sliderTrust">
-                    <li className="ts-active ts-trust-gray">
-                        <div className="ts-quote-top">
-                            <img src="//colt.trustvox.com.br/images/trust-quote-top.png" style={{float:"left"}} alt="quote" />
-                        </div>
-                        <div className="ts-opinion">
-                            <p></p>
-                            <small className="ts-opinion-author"></small>
-                            <small className="ts-opinion-date"></small>
-                            <div className="ts-trust-stars">
-                                <span className="ts-active-star"></span>
-                            </div>
-                        </div>
-                        <div className="ts-quote-bottom">
-                            <img src="//colt.trustvox.com.br/images/trust-quote-bottom.png" style={{float:"right"}} alt="quote" />
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <span className="ts-prev-slide">
-                <img src="//colt.trustvox.com.br/images/trust-left.png" className="ts-prev-button" alt="left" />
-            </span>
-            <span className="ts-next-slide">
-                <img src="//colt.trustvox.com.br/images/trust-right.png" className="ts-prev-button" alt="right" />
-            </span>
-        </div>
-        <div className="ts-audited">
-          Avaliações reais, auditadas por <img src="//colt.trustvox.com.br/images/trust-trustvox.png" className="ts-logo-trust" alt="Trustvox" />
-        </div>
-    </div>
-</div>
+<div id="_trustvox_colt"></div>
 
 export default SliderWidget
